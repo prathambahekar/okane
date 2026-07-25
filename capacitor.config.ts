3a9537c7e534d89e956717deef9a1dc779de488a
prathambahekar <prathambahekar97@gmail.com>
2026-07-25 18:21:57 +0530

    Initialize Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.okane.app',
+  appName: 'Okane',
+  webDir: 'dist'
+};
+
+export default config;
